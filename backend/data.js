5dfc6102881e0204c317d459ef9a59aa53f1a48a
jalmassi <justin.almassi@gmail.com>
2020-11-01 21:52:10 -0500

    add/remove items to cart

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default {
+    products: [
+        {
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirt',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 6
+    },
+    {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirt',
+        image: '/images/d1.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.2,
+        numReviews: 10,
+        countInStock: 5
+    },
+    {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 8,
+        countInStock: 10
+    },
+]
+}
