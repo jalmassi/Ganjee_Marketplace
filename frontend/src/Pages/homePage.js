c600d4f8a252677db2b074c190bb18a53d7d97c0
jalmassi <justin.almassi@gmail.com>
2020-12-02 21:05:01 -0500

    search products on home page

--- frontend/src/Pages/homePage.js
+++ frontend/src/Pages/homePage.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-// import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productsActions';
 import SearchBox from './SearchBox';
 
 function HomePage (props) {
 
-    // const [products, setProduct] = useState([]);
     const productList = useSelector(state => state.productList);
     const { products, loading, error } = productList;
     const dispatch = useDispatch();
     const [searchField, setSearchField] = useState('');
+    let searchProducts = products.filter(product =>(
+                product.name.toLowerCase().includes(searchField)));
+
     useEffect(() => { //runs when component rendered
         dispatch(listProducts());
         return () => {
             // cleanup
         };
-    }, [dispatch])
+    }, [dispatch, searchField])
 
     const _handleKeyDown = (e) => {
 
         if (e.key === 'Enter') {
-          console.log('Entered: ' + e.target.value);
           setSearchField(e.target.value);
         }
     }
@@ -30,29 +30,29 @@ function HomePage (props) {
     return (
     loading? <div>Loading...</div> :
     error? <div>{error}</div> :
-        <div>
-        <SearchBox placeholder="Search Product" handleChange={_handleKeyDown}/>
-        <ul className="products" >
-            {
-                products.map(product =>
-                <li key={product._id}>
-                    <div className="product">
-                        <Link to={'/product/' + product._id} >
-                            <img className="product-image" src={product.image} alt="product" />
-                        </Link>
-                        <div className="product-name">
-                            <Link to={'/product/' + product._id} >{product.name}</Link>
+            <div>
+            <SearchBox placeholder="Search Product" handleChange={_handleKeyDown}/>
+            <ul className="products" >
+                {
+                    searchProducts.map(product =>
+                    <li key={product._id}>
+                        <div className="product">
+                            <Link to={'/product/' + product._id} >
+                                <img className="product-image" src={product.image} alt="product" />
+                            </Link>
+                            <div className="product-name">
+                                <Link to={'/product/' + product._id} >{product.name}</Link>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">${product.price.toFixed(2)}</div>
+                            {/* <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div> */}
                         </div>
-                        <div className="product-brand">{product.brand}</div>
-                        <div className="product-price">${product.price.toFixed(2)}</div>
-                        {/* <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div> */}
-                    </div>
-                </li>
-                )
-            }
+                    </li>
+                    )
+                }
 
-        </ul>
-        </div>
+            </ul>
+            </div>
     )
 }
 
